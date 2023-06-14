37d4723656f69a9f7a58befdc9f5dc2e0da12bc3
taku.shiraishi <tshiraishi@unisrv.jp>
2023-06-14 15:38:13 +0900

    定数ファイルを作成

--- src/main/java/constants/PropertyConst.java
+++ src/main/java/constants/PropertyConst.java
@@ -1,5 +1,18 @@
 package constants;
 
 public enum PropertyConst {
+    
+  //ペッパー文字列
+    PEPPER("pepper");
+
+    private final String text;
+    private PropertyConst(final String text) {
+        this.text = text;
+    }
+
+    public String getValue() {
+//        セル単体の値取得
+        return this.text;
+    }
 
 }
