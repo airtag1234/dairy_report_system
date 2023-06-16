38ebfa198a11aea1fa308475792639e37626c754
taku.shiraishi <tshiraishi@unisrv.jp>
2023-06-16 13:34:04 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -154,6 +154,17 @@ public class EmployeeAction extends ActionBase {
         }
     }
 
+    public void destroy() throws ServletException, IOException {
+
+        if (checkToken()) {
+
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            redirect(ForwardConst.ACT_EMP,ForwardConst.CMD_INDEX);
+        }
+    }
+
     public void index() throws ServletException, IOException {
 
         int page = getPage();
